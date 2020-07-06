6b6705dff85f7ec9d4cee4f528d62367d067701e
jeremiahhauth <51054628+jeremiahhauth@users.noreply.github.com>
2020-07-06 15:29:31 -0400

    Upload of first attempt at layer 1 inference
    First attempt of running MFVI (flipout algorithm) on the first layer of U-NET. Additional tweeks to the main python script may be necessary. Script was run on 6 GPUs in parallel for 200 epochs. First glance shows training plateau at ~50 epochs, consistent with Snehal's deterministic model training.

---
+++ model_6_layer_1/batch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH -A precisionhealth_project2
+#SBATCH -p precisionhealth
+
+#SBATCH --job-name=model_6_layer_1
+#SBATCH --mail-user=hauthj@umich.edu
+#SBATCH --mail-type=END
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=32
+#SBATCH --mem-per-cpu=7g
+#SBATCH --time=24:00:00
+#SBATCH --gpus=6
+
+
+
+#SBATCH --output=/home/%u/tumor_segmentation_files/model_6_layer_1/%x-%j.log
+
+# The application(s) to execute along with its input arguments and options:
+
+python ./Layer_1_Bayesian.py
