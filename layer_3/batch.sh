b75b790f305925ad65e675529829acf28403b43d
Jeremiah Michael Axtell Hauth <hauthj@armis2-login1.arc-ts.umich.edu>
2020-07-07 11:14:37 -0400

    added layer 3 scripts ready to run

--- layer_3/batch.sh
+++ layer_3/batch.sh
@@ -3,18 +3,18 @@
 #SBATCH -A precisionhealth_project2
 #SBATCH -p precisionhealth
 
-#SBATCH --job-name= layer_3
+#SBATCH --job-name=layer_3
 #SBATCH --mail-user=hauthj@umich.edu
 #SBATCH --mail-type=END
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=32
 #SBATCH --mem-per-cpu=7g
 #SBATCH --time=24:00:00
-#SBATCH --gpus=4
+#SBATCH --gpus=3
 
 
 
-#SBATCH --output=/home/%u/tumor_segmentation_files/model_6_layer_3/%x-%j.log
+#SBATCH --output=/home/%u/tumor_segmentation_bayesian_analysis/layer_3/%x-%j.log
 
 # The application(s) to execute along with its input arguments and options:
 
