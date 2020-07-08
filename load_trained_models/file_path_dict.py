f48e51a68e66bc8e5ac14324490692f3082c153b
Jeremiah <hauthj@umich.edu>
2020-07-08 12:11:23 -0400

    adding load model scripts and path dict

---
+++ load_trained_models/file_path_dict.py
@@ -0,0 +1,14 @@
+file_path_dict = {
+'layer_1a': '../layer_1a/layer_1a_bayesian.h5',
+'layer_1b': '../layer_1b/layer_1b_bayesian.h5',
+'layer_2a': '../layer_2a/layer_2a_bayesian.h5',
+'layer_2b': '../layer_2b/layer_2b_bayesian.h5',
+'layer_3a': '../layer_3a/layer_3a_bayesian.h5',
+'layer_3b': '../layer_3b/layer_3b_bayesian.h5',
+'layer_4a': '../layer_4a/layer_4a_bayesian.h5',
+'layer_4b': '../layer_4b/layer_4b_bayesian.h5',
+'layer_5a': '../layer_5a/layer_5a_bayesian.h5',
+'layer_5b': '../layer_5b/layer_5b_bayesian.h5',
+}
+
+DATA_PATH = '../data/Task01_BrainTumour.h5'
