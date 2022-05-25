50088b503cb88d2ab11a541b5461168584a289c6
Jeremiah <hauthj@umich.edu>
2022-05-25 10:46:16 -0400

    practice deterministic file for gradient sensitivity

---
+++ layers/deterministic_v2/batch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#SBATCH -A precisionhealth_project2
+#SBATCH -p precisionhealth
+
+#SBATCH --job-name=encoder_1a
+#SBATCH --mail-user=hauthj@umich.edu
+#SBATCH --mail-type=END
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH --mem-per-cpu=7g
+#SBATCH --time=36:00:00
+#SBATCH --gpus=2
+
+
+
+#SBATCH --output=%x-%j.log
+
+# The application(s) to execute along with its input arguments and options:
+export LAYER_NAME="encoder_1a"
+echo "LAYER_NAME: "$LAYER_NAME
+python ./encoder_1a_bayesian.py
